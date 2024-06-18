b60c53c90ae2eafdbe0093d7bf7ae73514f93aaf
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-18 07:11:07 +0000

    task6

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - function
+*
+* Return: void
+**/
+
+int main(void)
+{
+	printf("Size of a char:%c", sizeof(char));
+	printf("Size of an int:%c", sizeof(int));
+	printf("Size of a long int:%c", sizeof(long int));
+	printf("Size of a long long int:%c", sizeof(long long int));
+	printf("Size of a float:%c", sizeof(float));
+	return (0);
+}
