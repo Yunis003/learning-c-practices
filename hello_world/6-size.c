adac8b554be3594e676811d834436201dbf5d829
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-18 07:20:05 +0000

    task6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char:%cu byte(s)\n", sizeof(char));
-	printf("Size of an int:%cu byte(s)\n", sizeof(int));
-	printf("Size of a long int:%cu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int:%cu byte(s)\n", sizeof(long long int));
-	printf("Size of a float:%cu byte(s)\n", sizeof(float));
+	printf("Size of a char:%zu byte(s)\n", sizeof(char));
+	printf("Size of an int:%zu byte(s)\n", sizeof(int));
+	printf("Size of a long int:%zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int:%zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float:%zu byte(s)\n", sizeof(float));
 	return (0);
 }
