6252e37789337dfca4c1b555008acc7c5759c364
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-22 09:09:49 +0000

    task5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 10; num++)
+	{
+		printf("num");
+	}
+	printf('\n');
+
+	return (0);
+}
