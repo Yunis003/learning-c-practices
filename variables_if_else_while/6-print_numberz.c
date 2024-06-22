0f454d5696129d52af2eeeb6c47bef803a344751
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-22 09:19:40 +0000

    task6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the single digit numbers of base 10,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+
+	return (0);
+}
