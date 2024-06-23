2cfd9901b7384daadf641a7158b116cfe9fc134b
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-23 08:31:01 +0000

    task5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,15 +8,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(5);
+		_putchar(43);
 		return (1);
 	} else if (n < 0)
 	{
-		_putchar(7);
+		_putchar(45);
 		return (-1);
 	}
 	else
-		_putchar(9);
+		_putchar(48);
 	return (0);
 
 }
