e09aa5f635a9b92f7f94f88c8f2ed230c73584f1
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-26 09:24:40 +0000

    task1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _isupper - function or entry point
+ * _isdigit - function or entry point
  * @c: character that will be checked
  * Return: 0 or 1 depends on the case
  **/
