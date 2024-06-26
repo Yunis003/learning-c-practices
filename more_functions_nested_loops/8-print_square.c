7770a5fc717b100654705180398facac5da917c6
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-26 11:58:33 +0000

    task8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - function or entry point
+ * @size: size of the square
+ **/
+
+void print_square(int size)
+{
+	int i, j;
+
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar('#');
+			_putchar('\n');
+		}
+		else
+			_putchar('\n')
+	}
+}
