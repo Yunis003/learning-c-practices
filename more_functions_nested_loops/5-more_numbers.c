11f70ba68eb1e78068cbaacfd75fae6e581d01a6
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-26 10:19:16 +0000

    task5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - function or entry point
+ * Return: expected result for function
+ **/
+
+void more_numbers(void)
+{
+	int a = 0, j;
+
+	while (a <= '10')
+	{
+		_putchar('\n');
+		a++;
+
+		j = '0'
+		while (j <= '14')
+		{
+			_putchar(j);
+			j++
+		}
+	}
+	_putchar('\n')
+}
