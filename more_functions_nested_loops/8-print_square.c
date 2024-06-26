1bc3de4d4f44c63b85dde0401695eef8db082f3f
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-26 12:00:05 +0000

    task8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -9,14 +9,13 @@ void print_square(int size)
 	int i, j;
 
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= size; i++)
 	{
 		for (j = 1; j <= i; j++)
 		{
 			_putchar('#');
 			_putchar('\n');
 		}
-		else
-			_putchar('\n')
+		_putchar('\n');
 	}
 }
