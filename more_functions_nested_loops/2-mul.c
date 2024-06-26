d1a37f8028c9dea98f4a8b1cf4cae18ff9910115
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-26 09:25:51 +0000

    task2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,13 +1,10 @@
 #include "main.h"
 /**
- * _isupper - function or entry point
+ * mul - function or entry point
  * @c: character that will be checked
  * Return: 0 or 1 depends on the case
  **/
 int mul(int a, int b)
 {
-	char c;
-
-	c = a + b;
-	return (c);
+	return (a + b);
 }
