e236f933432cb0726007eebc0a1fef24ebdcb222
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-26 12:10:47 +0000

    task8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -3,24 +3,21 @@
  * print_square - function or entry point
  * @size: size of the square
  **/
-
 void print_square(int size)
 {
 	int i, j;
-
 
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j=0; j < size; j++)
-			{
+			for (j = 0; j < size; j++)
 				_putchar('#');
-				_putchar('\n');
-			}
+		          	_putchar('\n');
 		}
 	}
 	else
+	{
 		_putchar('\n');
-	
+	}
 }
