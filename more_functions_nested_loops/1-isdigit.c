649fcc3ff8a108dfca22bbba6d4ffc5f0edc5521
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-26 09:07:51 +0000

    task1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - function or entry point
+ * @c: character that will be checked
+ * Return: 0 or 1 depends on the case
+ **/
+int _isdigit(int c)
+{
+	if (c >= '1' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
