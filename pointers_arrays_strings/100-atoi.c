5b791faf8cc7967cff8e756a331171ae23c6f3c1
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-30 12:26:17 +0000

    task10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _atoi - function that convert a string to an integer;
+* Return: num * sign
+* @s: given string
+**/
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+	if (*s == '-')
+	{
+	sign *= -1;
+	}
+	else if (*s >= '0' && *s <= '9')
+	{
+	num = (num * 10) + (*s - '0');
+	}
+	} while (*s++);
+	return (num * sign);
+}
