29a033da09a4abe1e3612a13d44fff0ef772b4de
Yunis003 <hi.yunisqulamov@gmail.com>
2024-06-30 12:13:29 +0000

    task5

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
- * _puts - prints a string followed by a new line to stdout
- * @str: the string to be printed
- */
+* _puts - prints string followed
+* by a new line to standart output
+* @str: string parameter
+**/
 void _puts(char *str)
 {
-	while (*str)
+	int string;
+
+	for (string = 0; str[string] != '\0'; string++)
 	{
-		_putchar(*str++);
+	_putchar(str[string]);
 	}
 	_putchar('\n');
 }
