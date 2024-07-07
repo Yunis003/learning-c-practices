f81a69ef8fda91a03b6632cceb480c47f7cb2983
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-07 14:39:28 +0000

    task2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -23,4 +23,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, const char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
