4828a8ae86f322afadbe9ff8039930997f9fcc2c
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-07 17:16:31 +0000

    task4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion(int x, int y)- is the main function
+ * x-first num
+ * y- powered num
+ * Return The result of x raised to the power of y,
+ *         or -1 if y is less than 0
+ * **/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
