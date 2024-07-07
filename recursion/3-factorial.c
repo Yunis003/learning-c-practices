3f77abfb45844245f690216d8997cb8a52e574d7
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-07 17:08:01 +0000

    task3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial(int n) is the main function
+ * n is the integer which is the must fin the integral
+ * **/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return n * factorial(n-1);
+}
