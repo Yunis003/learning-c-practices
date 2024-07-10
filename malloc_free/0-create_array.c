e9d714faf7796168039c5242517d2da9a5be8193
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-10 09:06:10 +0000

    task0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array is the array creater
+ * Returns NULL if size = 0
+Returns a pointer to the array, or NULL if it fails
+ * **/
+
+char *create_array(unsigned int size, char c)
+{
+	char arr[];
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	
+	for(c = 0x48; i <= size; i++)
+	{
+		arr[] = c;
+	        return (0);
+	}
+	return (0);
+}
