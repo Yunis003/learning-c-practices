89a5896b2ba82cf4fae2f21ebb88e653c86afcc8
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-10 12:10:06 +0000

    task2_update1

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+/**
+ *return
+ * **/
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	unsigned char l, k, i;
+
+	if (s1 == NULL)
+	{
+		s1 = " ";
+	}
+	if (s2 == NULL)
+	{
+		s2 = " ";
+	}
+	for (l = 0; s1[l] != '\0'; l++ )
+	{
+		;
+	}
+	for (k = 0; s2[k] != '\0'; k++)
+	{
+		;
+	}
+	concat = malloc(sizeof(char) * (l + 1) * (k + 1));
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= l; i++)
+	{
+ 		concat[i] = s1[i] + s2[i];
+	}
+	return (concat);
+}
