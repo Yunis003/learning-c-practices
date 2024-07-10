fdd5ac51e7b46e2c7aad09e6c0d6379a6d0d88c4
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-10 09:41:23 +0000

    task0_update1

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,13 +11,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	
 	if (size == 0)
 	{
 		return (NULL);
 	}
-
-	array = malloc(size * sizeof(char));
+	array = malloc (sizeof(char) * size);
 	if (array == NULL)
 	{
 		return (NULL);
