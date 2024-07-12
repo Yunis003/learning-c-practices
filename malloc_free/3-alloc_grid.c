f98c209bc64bd6116ffc9b6d7cb3694d9dd5cd55
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-12 10:48:26 +0000

    task3_ypdate300

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,7 +21,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	for (i = 0; i <= height; i++)
+	for (i = 0; i < height; i++)
 	{
 		array[i] = malloc(width * sizeof(int *));
 		if (array[i] == NULL)
@@ -33,7 +33,7 @@ int **alloc_grid(int width, int height)
 			free(array);
 			return (NULL);
 		}
-		for (j = 0; j <= width; j++)
+		for (j = 0; j < width; j++)
 		{
 			array[i][j] =  0;
 		}
