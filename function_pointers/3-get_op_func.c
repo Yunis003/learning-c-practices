4b29cdbdc2a8409f11dc6b5dd053bcebdd377649
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-20 09:04:05 +0000

    task_final_update6

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -12,16 +12,16 @@ int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
 
-	 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-    while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
 		i++;
 	}
