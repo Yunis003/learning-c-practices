3b86ce12b4ca4246caec5edb72125acc25e3f081
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-21 15:00:05 +0000

    tasks_updated

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,18 +2,15 @@
 #define VARIADIC_FUNCTIONS_H
 #include <stdlib.h>
 #include <stdarg.h>
-
-
-typedef struct print 
+typedef struct print
 {
 	char *type;
 	void (*f)(va_list ap);
 }print_type;
 
-
 int sum_them_all(const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif
