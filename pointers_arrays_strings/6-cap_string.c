2e6199584f2d7667284c330511a632f0686086a7
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-21 19:03:26 +0000

    update

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -18,11 +18,13 @@ char *cap_string(char *str)
 		{
 			str[index] =  str[index] - conversion;
 		}
+		conversion = 0;
 		for (count = 0; chars[count] != '\0'; count++)
 		{
 			if (chars[count] == str[index])
 			{
 				conversion = 32;
+				break;
 			}
 		}
 	}
