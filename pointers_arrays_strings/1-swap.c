86da85b656de7d3cdf62c426c41380b0cddb7dc0
Yunis003 <hi.yunisqulamov@gmail.com>
2024-07-21 19:05:53 +0000

    update

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- * void swap_int(int *a, int *b)
- **/
+* swap_int - function or entry point
+* @a: first parameter
+* @b: second parameter
+**/
 void swap_int(int *a, int *b)
 {
-	int c = *a;
+	int c;
 
+	c = *a;
 	*a = *b;
 	*b = c;
 }
