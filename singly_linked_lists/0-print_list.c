82eb0d5e4ee4fdb99aaa2b69d5f93b75ec903eb4
Yunis003 <hi.yunisqulamov@gmail.com>
2024-08-02 09:52:45 +0000

    task-

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+/**
+ * print_list - prints all elements of the list_t list
+ * @h: pointer to our head node
+ * Return: count of the nodes
+ **/
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		if ((*h).str == NULL)
+		{
+			printf("[0] (nil)/n");
+		}
+		else 
+		{
+			printf("[%d]/n %s/n", (*h).len, (*h).str);
+		}
+		h = (*h).next;
+		counter++;
+	}
+	return (counter);
+}
